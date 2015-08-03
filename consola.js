a5d9714d2e44d9f2953a77aa284167d5bb1264da
DazLamas <sarah.d.lamas@gmail.com>
2015-08-03 23:49:22 +0200

    Initial

---
+++ consola.js
@@ -0,0 +1,5 @@
+/* Write here yout elements:                */
+
+var is_this_empty = document.getElementById('emptyness_validation');
+
+isEmpty(is_this_empty);
---
+++ justine.js
@@ -0,0 +1,12 @@
+// alert('Justine is here');
+
+function isEmpty(element) {
+
+  console.log('dentro');
+  console.log(element);
+
+  if( element.value == 0 ){
+    alert('is empty');
+  }
+
+}
