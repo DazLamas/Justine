f285c8ae44e0c35daa2b4b058fadbbb5c77e3a60
JOSE CARLOS GARCIA CASAS <mariagarciasuero@MacBook-Pro-de-JOSE-CARLOS-GARCIA-CASAS.local>
2015-08-12 01:26:37 +0200

    First flow

--- Elements.js
+++ Elements.js
@@ -1,12 +1,12 @@
-/* Write here your elements:                */
+// /* Write here your elements:                */
 
-var emptyness_validation = document.querySelectorAll("[data-form-validation='emptyness_validation']");
-// var validation_btn       = document.querySelectorAll("[data-funtion='send-data']");
+// var emptyness_validation = document.querySelectorAll("[data-form-validation='emptyness_validation']");
+// // var validation_btn       = document.querySelectorAll("[data-funtion='send-data']");
 
-document.getElementById('validation-btn').addEventListener('click', fireValidations, false);
+// document.getElementById('validation-btn').addEventListener('click', fireValidations, false);
 
-function setEmptynessValidationToElements() {
-  for (var i = 0; i < emptyness_validation.length; i++) {
-    isEmpty(emptyness_validation[i]);
-  };
-};
+// function setEmptynessValidationToElements() {
+//   for (var i = 0; i < emptyness_validation.length; i++) {
+//     isEmpty(emptyness_validation[i]);
+//   };
+// };
--- justine.js
+++ justine.js
@@ -1,18 +1,76 @@
-console.log('Justine is here, girls');
+console.log('Justine is here');
 
 //Here we go!
 ///////////////////////////////////////
 
-function isEmpty(element) {
+function emptynessValidation(element) {
 
   if(!element.value){
     alert('is empty');
-  }
-  if(element.value){
+  }else{
+    (element.value)
     alert('has value');
   }
 };
 
-function fireValidations() {
-  setEmptynessValidationToElements();
+//Fire validations up
+var fire_validations_element = document.getElementsByClassName("validation-btn");
+
+for(var i=0;i<fire_validations_element.length;i++){
+
+    fire_validations_element[i].addEventListener('click', whichForm, false);
+
 };
+
+function whichForm(event) {
+  event.preventDefault();
+  // var form_id = event.target.classList[1];
+  var button              = event.target;
+  var parent_element      = button.parentNode;
+  var what_im_looking_for = 'form';
+
+  while (parent_element.tagName.toLowerCase() != what_im_looking_for) {
+    parent_element = parent_element.parentNode;
+  }
+
+  lookForValidations(parent_element);
+
+};
+
+function lookForValidations(form) {
+  
+  var justine_elements = form.getElementsByClassName('justine');
+
+  for (var i = justine_elements.length - 1; i >= 0; i--) {
+    var what_validation = justine_elements[i].classList[1];
+    fireValidation(what_validation, justine_elements[i]);
+  };
+
+};
+
+function fireValidation(function_name, function_argument){
+  
+  var fn = window[function_name];
+
+  if(typeof fn === 'function') {
+      fn(function_argument);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
