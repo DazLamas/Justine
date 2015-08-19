3fcf21bc5c4aaf8fdb32e2a8b47b4c50ed609dc0
DazLamas <sarah.d.lamas@gmail.com>
2015-08-19 22:03:34 +0200

    Get validations from classes in any position; add prefix to classes

--- justine.js
+++ justine.js
@@ -2,6 +2,15 @@ console.log('Justine is here');
 
 //Here we go!
 ///////////////////////////////////////
+function onlyNumbers(element) {
+   var error_message = 'Solo números';
+  if(!isNaN(element))
+  {
+    createError(element, error_message);
+  }else{
+    console.log('is a number');
+  }
+}
 
 function emptynessValidation(element) {
   
@@ -10,27 +19,11 @@ function emptynessValidation(element) {
   if(!element.value){//error
     createError(element, error_message);
   }else{
-    (element.value)
-    alert('has value');
+    console.log('is not empty');
+    return true
   }
 };
 
-function createError(element, error_message) {
-  
-  var parent_node 	= element.parentNode;
-  var new_element	= document.createElement("span");
-
-  if(parent_node.lastchild == element) {
-  
-  	var error_container = parent_node.appendChild(new_element);
-  } else {
-  
-  	var error_container = parent_node.insertBefore(new_element, element.nextSibling);
-  }
-
-  error_container.classList.add('error');
-  error_container.innerHTML = error_message;
-}
 
 //Fire validations up
 var fire_validations_element = document.getElementsByClassName("validation-btn");
@@ -42,8 +35,9 @@ for(var i=0;i<fire_validations_element.length;i++){
 };
 
 function whichForm(event) {
+  
   event.preventDefault();
-  // var form_id = event.target.classList[1];
+  
   var button              = event.target;
   var parent_element      = button.parentNode;
   var what_im_looking_for = 'form';
@@ -52,19 +46,34 @@ function whichForm(event) {
     parent_element = parent_element.parentNode;
   }
 
-  lookForValidations(parent_element);
+  getFormDomElements(parent_element);
 
 };
 
-function lookForValidations(form) {
+function getFormDomElements(form) {
   
-  var justine_elements = form.getElementsByClassName('justine');
+  form_children = form.childNodes;
 
-  for (var i = justine_elements.length - 1; i >= 0; i--) {
-    var what_validation = justine_elements[i].classList[1];
-    fireValidation(what_validation, justine_elements[i]);
+  for (var i = 0; i < form_children.length; i++) {
+    
+    if(form_children[i].nodeType == 1){
+      getJustineElementsAndValidations(form_children[i]);
+    };
   };
 
+};
+
+function getJustineElementsAndValidations(form_dom_element) {
+
+  var form_child_classes = form_dom_element.className.split(' ');
+
+      for (var i = 0; i < form_child_classes.length; i++) {
+
+        if (form_child_classes[i].indexOf('justine-') !== -1) {
+          var function_name = form_child_classes[i].substring(form_child_classes[i].indexOf("-") + 1);
+          fireValidation(function_name, form_children[i]);
+        };
+      };
 };
 
 function fireValidation(function_name, function_argument){
@@ -76,6 +85,24 @@ function fireValidation(function_name, function_argument){
   }
 };
 
+function createError(element, error_message) {
+  console.log(element);
+  // var parent_node  = element.parentNode;
+  // var new_element  = document.createElement("span");
+
+  // if(parent_node.lastchild == element) {
+  
+  //  var error_container = parent_node.appendChild(new_element);
+  // } else {
+  
+  //  var error_container = parent_node.insertBefore(new_element, element.nextSibling);
+  // }
+
+  // error_container.classList.add('error');
+  
+  var error_container       = document.getElementById('errors-display');
+  error_container.innerHTML = error_message;
+}
 
 
 
